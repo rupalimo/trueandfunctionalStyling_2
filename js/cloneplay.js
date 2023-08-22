5bf173695611307de1e9ab570eb1e658baa9cf54
e tanaka <elina.tanaka@gmail.com>
2023-08-22 15:36:43 -0700

    loading screen

--- js/cloneplay.js
+++ js/cloneplay.js
@@ -109,30 +109,29 @@ $("#delete").on("click", function () {
 document.addEventListener("DOMContentLoaded", function () {
     var div = document.getElementById("endlessscroll");
     div.addEventListener("scroll", function () {
-      var max_scroll = this.scrollHeight - this.clientHeight;
-      var current_scroll = this.scrollTop;
-      var bottom = 100;
-      if (current_scroll + bottom >= max_scroll) {
-        var ul = document.getElementsByTagName("ul")[0];
-        var current = parseInt(ul.dataset.current, 10);
-        var li = document.getElementsByTagName("li")[current];
-        var new_li = li.cloneNode(true);
-        ul.appendChild(new_li);
-        ul.dataset.current = current + 1;
-      }
+        var max_scroll = this.scrollHeight - this.clientHeight;
+        var current_scroll = this.scrollTop;
+        var bottom = 100;
+        if (current_scroll + bottom >= max_scroll) {
+            var ul = document.getElementsByTagName("ul")[0];
+            var current = parseInt(ul.dataset.current, 10);
+            var li = document.getElementsByTagName("li")[current];
+            var new_li = li.cloneNode(true);
+            ul.appendChild(new_li);
+            ul.dataset.current = current + 1;
+        }
     });
-  });
+});
 
 
 // index toggle
 var indButton = document.getElementById('indexToggle');
 
-indButton.onclick = function() {
+indButton.onclick = function () {
     var inDiv = document.getElementById('indDisplay');
     if (inDiv.style.display === 'none') {
         inDiv.style.display = 'block';
-    }
-    else {
+    } else {
         inDiv.style.display = 'none';
     }
 };
